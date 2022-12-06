448f4ef1b2851805774ebf09cdf43c50f4f147a9
ravindrakuna226 <109706136+ravindrakuna226@users.noreply.github.com>
2022-12-06 18:42:21 +0530

    Update ec2creation.tf

--- ec2creation.tf
+++ ec2creation.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   region     = "ap-south-1"
 }
-resource "aws_instance" "project1" {
+resource "aws_instance" "test_server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
-  key_name = "test5.pem"
-   subnet_id = "subnet-0c572dd5617259633"
-   security_groups = ["sg-0d9cb8ac647675bb9"]
+  key_name = "test5"
   tags = {
     Env = "testing"
-    Name = "project1"
+    Name = "test_server"
   }
 }

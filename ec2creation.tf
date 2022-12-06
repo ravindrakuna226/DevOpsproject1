d73f4e21442b150de2da68bd590ae0500cd540ba
ravindrakuna226 <109706136+ravindrakuna226@users.noreply.github.com>
2022-12-06 19:30:53 +0530

    Update ec2creation.tf

--- ec2creation.tf
+++ ec2creation.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "test_server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
   key_name = "test5"
+   subnet_id = "subnet-0c572dd5617259633"
+   security_groups = ["sg-0d9cb8ac647675bb9"]
   tags = {
     Env = "testing"
     Name = "test_server"
